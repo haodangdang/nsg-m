fc716f9569671666190f86d90acee50f7e9c8337
Howe <hhhaohao@163.com>
2020-11-29 21:55:03 +0800

    new

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
 var main = {
 	gameSwiper: null,
 	init: function () {
+		this.initPage();
 		this.bind();
-		// this.initHero();
+		this.initHero();
 		this.initFeature();
-		// this.initHomePlayBtn();
+		this.initHomePlayBtn();
+	},
+	initPage: function () {
+		var self = this;
+		self.pageSwiper = new Swiper ('.page_warp', {
+		    direction: 'vertical',
+		    spaceBetween: 0,
+        	slidesPerView: 1,
+        	mousewheel: true,
+        	freeModeSticky : true,
+	        on: {
+	        	slideChangeTransitionStart: function () {
+	        		$('.nav_item').removeClass('current');
+	        		$('.nav_item').eq(this.activeIndex).addClass('current');
+	            }
+	        }
+		}); 
 	},
 	initHomePlayBtn: function () {
 		var player = new SVGA.Player('#homePlay');
@@ -15,23 +32,23 @@ var main = {
 		})
 	},
 	initHero: function () {
-		// var heroSwiper = new Swiper ('.hero_list', {
-		//     autoplay: {
-	 //            delay: 5000,
-	 //            disableOnInteraction: false
-	 //        },
-	 //        loop: true,
-	 //        centeredSlides: true,
-	 //        spaceBetween: 0,
-	 //        slidesPerView: "auto",
-	 //        speed: 1000,
-	 //        touchStartForcePreventDefault : false,
-	 //        mousewheel: false,
-	 //        mousewheel: {
-	 //            forceToAxis: true,
-	 //        },
-	 //        loopedSlides: 5
-		// }); 
+		var heroSwiper = new Swiper ('.hero_list', {
+		    autoplay: {
+	            delay: 5000,
+	            disableOnInteraction: false
+	        },
+	        loop: true,
+	        centeredSlides: true,
+	        spaceBetween: 0,
+	        slidesPerView: "auto",
+	        speed: 1000,
+	        touchStartForcePreventDefault : false,
+	        mousewheel: false,
+	        mousewheel: {
+	            forceToAxis: true,
+	        },
+	        loopedSlides: 5
+		}); 
 		var heroHeadSwiper = new Swiper ('.hero_head', {
 		    loop: true,
 	        slideToClickedSlide: true,
@@ -50,8 +67,8 @@ var main = {
 	        loopedSlides: 5,
 	        grabCursor: true,
 		}); 
-		// heroSwiper.controller.control = heroHeadSwiper;
-	 //    heroHeadSwiper.controller.control = heroSwiper;
+		heroSwiper.controller.control = heroHeadSwiper;
+	    heroHeadSwiper.controller.control = heroSwiper;
 	},
 	initFeature: function () {
 		this.gameSwiper = new Swiper('.feature_list', {
@@ -72,8 +89,17 @@ var main = {
 		var self = this;
 		$('.feature_title_item').on('click', function () {
 			self.gameSwiper.slideTo($(this).index() + 1, 500, true);
-		})
+		});
+		$('.app_download, .download').on('click', function () {
+			self.showDownloadModal();
+		});
 	},
+	showDownloadModal: function () {
+		$('.download_modal').show();
+	},
+	hideDownloadModal: function () {
+		$('.download_modal').hide();
+	}
 }
 main.init();
 
